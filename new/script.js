39f8fd0452645ea4ea48c266273d058abe42c74a
Ilaha <fttkpsttkp@gmail.com>
2023-04-27 11:33:56 +0400

    upload

--- new/script.js
+++ new/script.js
@@ -53,14 +53,14 @@ const student = {
     
 function handleObjects(obj, key, action){
     if(action=='get'){
-     return obj[key];
+    return obj[key];
     }
     else if(action=='add'){
         obj[key]="";
         return obj;
     }
     else if(action=='delete'){
-         delete obj[key];
+        delete obj[key];
         return obj;
     }
     else {
@@ -70,10 +70,69 @@ function handleObjects(obj, key, action){
     const result = handleObjects(student, 'programmingLanguage', 'delete');
     console.log('result', result);*/
 
-    //task 4
-    function getKiller(suspectInfo, deadPeople){
-        for(let i=0; i<suspectInfo.length; i++)
-        {if(suspectInfo[i]==deadPeople[i])
-            {}
+//task 4
+/*function getKiller(suspectInfo, deadPeople) {
+    for (let key in suspectInfo) {
+        let k = 0;
+
+        for (let n of deadPeople) {
+            if (suspectInfo[key].includes(n)) {
+                k++;
+            }
+        }
+
+        if (k == deadPeople.length) {
+        return key;
         }
     }
+}
+getKiller(
+    {
+        'James': ['Jacob', 'Bill', 'Lucas'],
+        'Johnny': ['David', 'Kyle', 'Lucas'],
+        'Peter': ['Lucy', 'Kyle'],
+    }, ['Lucas', 'Bill']
+); // Killer James
+
+getKiller(
+    {
+        'Brad': [],
+        'Megan': ['Ben', 'Kevin'],
+        'Finn': [],
+    },
+    ['Ben']
+); // Killer Megan
+*/
+//fifth task
+//function getWinner(applicants, winnerObject){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//date excercises
+//1)
+/*function getDateFormst(date, separator){
+date = new Date();
+    let df = date.getFullYear() + separator + ('0' + (date.getMonth()+1)).slice(-2) + separator + date.getDate();
+}*/
+
+//2)
+/*function getDaysBeforeBirthday(bday){
+let date=new Date();
+bday=new Date(date.getFullYear(), 8, 15);
+bday=Math.ceil( (bday.getTime()-date.getTime()) / (1000 * 3600 * 24));
+}
+*/
+
